d0b472b191b4821707bc8f26e302a6b43f0dd4a3
Rob Ashton <robashton@codeofrob.com>
2011-03-03 20:37:19 +0000

    Added the libraries I'm using to do all that RSS niceness

---
+++ rob-rss.js
@@ -0,0 +1,42 @@
+var xml = require('./node-xml');
+
+exports.parse = function(itemCallback)
+{
+ return new xml.SaxParser(function(cb){
+                                        var inItem = false;
+                                        var contentBuffer = '';
+                                        var currentItem = {};
+                                        cb.onStartDocument(function(){
+                                        });
+                                        cb.onEndDocument(function(){
+                                        });
+                                        var isLink = false;
+                                        cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
+                                                if(elem == 'item' || elem == 'entry') { inItem = true;}
+                                        });
+                                        cb.onCharacters(function(chars){
+                                                if(inItem) { contentBuffer += chars; }
+                                        });
+                                        cb.onCdata(function(chars){
+                                                if(inItem) { contentBuffer += chars; }
+                                        });
+                                        cb.onEndElementNS(function(elem, attrs, prefix) {
+                                                if(inItem) {
+                                                        if(elem == 'item' || elem == 'entry')
+                                                        {
+                                                                inItem = false;
+                                                                itemCallback(currentItem);
+                                                                currentItem = {};
+                                                        }
+                                                        else
+                                                        {
+                                                                currentItem[elem] = contentBuffer;
+                                                        }
+                                                contentBuffer = '';
+                                                }
+                                        });
+
+                                });
+};
+
+
